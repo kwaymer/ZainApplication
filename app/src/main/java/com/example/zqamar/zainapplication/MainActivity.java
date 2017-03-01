0734d60401542e22ac83f17c8c63192bd08f6573
Kwaymer <zqamar1@student.gsu.edu>
2017-03-01 18:43:33 -0500

    - Application is runnable, except i get an error called EGL_SWAP_BEHAVIOR

--- app/src/main/java/com/example/zqamar/zainapplication/MainActivity.java
+++ app/src/main/java/com/example/zqamar/zainapplication/MainActivity.java
@@ -1,25 +1,37 @@
 package com.example.zqamar.zainapplication;
+
 import android.content.Intent;
 import android.os.Bundle;
-import android.support.v7.app.AppCompatActivity;
 import android.view.View;
+import android.view.animation.Animation;
 import android.widget.ImageButton;
 
-public class MainActivity extends AppCompatActivity {
+import butterknife.OnClick;
+
+public class MainActivity extends BaseActivity {
 
     private ImageButton topRightButton;
+    private ImageButton topLeftButton;
     private ImageButton bt1;
     private ImageButton bt3;
+
+    @OnClick
+    public void toAnimation() {
+        toAnimation(AnimationActivity.class);
+
+    }
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         initialView();
-        initalListener();
+        initialListener();
     }
 
-    private void initalListener(){
+    private void initialListener(){
 
 
         topRightButton.setOnClickListener(new View.OnClickListener() {
@@ -32,6 +44,12 @@ public class MainActivity extends AppCompatActivity {
     }
 
     private void initialView() {
+        bt1 = (ImageButton)findViewById(R.id.bt1);
+        bt3 = (ImageButton)findViewById(R.id.bt3);
+        topLeftButton = (ImageButton)findViewById(R.id.topLeftButton);
         topRightButton = (ImageButton)findViewById(R.id.topRightButton);
     }
+    }
+
+
 }
