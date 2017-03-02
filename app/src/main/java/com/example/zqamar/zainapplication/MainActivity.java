152cc6c9423e5dbf2e58b22e1a61fe31b997bd7b
Kwaymer <zqamar1@student.gsu.edu>
2017-03-01 19:11:09 -0500

    - Application fully runnable, HW2 and HW3 are finished, currently fixing animation

--- app/src/main/java/com/example/zqamar/zainapplication/MainActivity.java
+++ app/src/main/java/com/example/zqamar/zainapplication/MainActivity.java
@@ -3,9 +3,11 @@ package com.example.zqamar.zainapplication;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
-import android.view.animation.Animation;
+import android.widget.Button;
 import android.widget.ImageButton;
+import android.widget.Toast;
 
+import butterknife.ButterKnife;
 import butterknife.OnClick;
 
 public class MainActivity extends BaseActivity {
@@ -15,11 +17,18 @@ public class MainActivity extends BaseActivity {
     private ImageButton bt1;
     private ImageButton bt3;
 
-    @OnClick
-    public void toAnimation() {
-        toAnimation(AnimationActivity.class);
+    private Button timer;
 
+    //@OnClick(R.id.bt_animation)
+    //public void toAnimation() {
+     //   toAnimation(AnimationActivity.class);
+
+    @OnClick(R.id.bt2)
+    public void button2Click(){
+        Intent i = new Intent(this, DialogActivity.class);
+        startActivityForResult(i, 2);
     }
+
 
 
 
@@ -29,9 +38,48 @@ public class MainActivity extends BaseActivity {
         setContentView(R.layout.activity_main);
         initialView();
         initialListener();
+        ButterKnife.bind(this);
     }
 
     private void initialListener(){
+        bt1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(v.getContext(), "Button 1", Toast.LENGTH_SHORT).show();
+
+                Intent i = new Intent(v.getContext(), ViewPagerActivity.class);
+                i.putExtra("key", "value");
+                Bundle b = new Bundle();
+                b.putInt("Ingeter", 12345);
+
+                Book book = new Book();
+                book.setName("Book's Name");
+                book.setAuthor("Book's Author");
+                b.putSerializable("book", book);
+
+                i.putExtras(b);
+                startActivityForResult(i, 1);
+
+            }
+        });
+
+        bt3.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                toastShort("Button 3");
+
+                Intent i = new Intent(v.getContext(), ListViewActivity.class);
+                startActivityForResult(i, 3);
+
+            }
+        });
+
+        topLeftButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+            }
+        });
 
 
         topRightButton.setOnClickListener(new View.OnClickListener() {
@@ -49,7 +97,12 @@ public class MainActivity extends BaseActivity {
         topLeftButton = (ImageButton)findViewById(R.id.topLeftButton);
         topRightButton = (ImageButton)findViewById(R.id.topRightButton);
     }
+
+    public void onClick(View v){
+        Toast.makeText(this,"Button 2 was Clicked", Toast.LENGTH_SHORT).show();
+    }
+
     }
 
 
-}
+
